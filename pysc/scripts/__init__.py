6de8ea7246ba35869b0ab354d99dd5f1e0177971
Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>
2019-03-27 14:29:33 +0100

    added script to assemble mixcr results
    Signed-off-by: Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>

--- pysc/scripts/__init__.py
+++ pysc/scripts/__init__.py
@@ -1,1 +1,2 @@
 from .demultiplex import demultiplex
+from .assemble_mixcr import assemble_mixcr
---
+++ pysc/scripts/assemble_mixcr.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import os.path
+import gzip
+from operator import itemgetter
+from itertools import groupby
+
+
+class Table:
+
+    def __init__(self, stream):
+        self.stream = stream
+        for line in self.stream:
+            line = line.rstrip("\n")
+            self.header = line.split("\t")
+            break
+
+    def __iter__(self):
+        for line in self.stream:
+            line = line.rstrip("\n")
+            yield line.split("\t")
+
+
+def assemble_mixcr(args):
+    """Assemble MIXCR reports."""
+    outstream = sys.stdout
+    if args.output != "stdout":
+        if args.output.endswith(".gz"):
+            outstream = gzip.open(args.output, "wt")
+        else:
+            outstream = open(args.output, "wt")
+
+    # process the reports
+    for pidx, path in enumerate(args.reports):
+
+        # check if the file is present
+        if not os.path.exists(path):
+            sys.stderr.write("Error: could not find file {0}\n".format(path))
+            continue
+
+        # get the data from the file
+        stream = open(path, "rt")
+        table = Table(stream)
+        data = [row for row in table]
+        stream.close()
+
+        # just concatenate the files and skip all the processing steps
+        if args.no_processing:
+            if pidx == 0:
+                outstream.write("{0}\n".format("\t".join(table.header)))
+            for row in data:
+                outstream.write("{0}\n".format("\t".join(row)))
+            continue
+
+        # add the file name
+        fname = os.path.basename(path)
+        types = []
+        for row in data:
+            ctype = row[5][:3]
+            cscore = float(row[1])
+            types.append((ctype, cscore))
+
+        # get the total and maximum per type
+        totaltype = {}
+        maxtype = {}
+        for tpl in types:
+            if tpl[0] not in totaltype.keys():
+                totaltype[tpl[0]] = 0.0
+            totaltype[tpl[0]] += tpl[1]
+
+            if tpl[0] not in maxtype.keys():
+                maxtype[tpl[0]] = 0.0
+            if tpl[1] > maxtype[tpl[0]]:
+                maxtype[tpl[0]] = tpl[1]
+        total = sum(tpl[1] for tpl in types)
+
+        # write the header for the first file
+        if pidx == 0:
+            fields = [
+                "file name",
+                "type",
+                "fraction of total",
+                "fraction of type",
+                "fraction of maximum of type"]
+            fields.extend(table.header)
+            outstream.write("{0}\n".format("\t".join(fields)))
+
+        # write the content
+        for idx, (ctype, score) in enumerate(types):
+            line = "{file}\t{type}\t{ftotal}\t{ftype}\t{fmax}\t{row}\n".format(
+                file=fname,
+                type=ctype,
+                ftotal=score/total,
+                ftype=score/totaltype[ctype],
+                fmax=score/maxtype[ctype],
+                row="\t".join(data[idx]))
+            outstream.write(line)
+
+    # close the output file
+    if outstream is not sys.stdout:
+        outstream.close()
