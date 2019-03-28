d5cfd4e07bbce7be368a7e1bc804a4e33732fd8a
Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>
2019-03-28 14:34:00 +0100

    debugging
    Signed-off-by: Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>

--- pysc/scripts/add_sam_tags.py
+++ pysc/scripts/add_sam_tags.py
@@ -6,6 +6,48 @@ import pyngs.sam as sam
 from pysc import read_well_list
 
 
+class Annotate:
+
+    def __init__(self, expected):
+        self.expected = expected
+        self.wbcidx = 7
+        self.umiidx = -1
+
+    def add_tags(self, instream, outstream):
+        """."""
+        reader = sam.Reader(instream)
+        writer = sam.Writer(outstream, reader.header)
+        for alignment in reader:
+
+            # get the well barcode
+            fields = alignment.name.split(":")
+            wbc = fields[self.wbcidx].replace("wbc=", "", 1)
+
+            # add the well barcode
+            tags = [("bc", "Z", wbc)]
+
+            # add the UMI if specified
+            if self.umiidx >= 0:
+                umi = fields[self.umiidx].replace("umi=", "", 1)
+                tags.append(("um", "Z", umi))
+
+            # get the well list information
+            if wbc in self.expected.keys():
+                anno = self.expected[wbc]
+                tags.extend([
+                    ("sm", "Z", anno["sample"]),
+                    ("rw", "Z", anno["row"]),
+                    ("cl", "Z", anno["column"])])
+            else:
+                tags.extend([
+                    ("sm", "Z", "unassigned"),
+                    ("rw", "Z", "-1"),
+                    ("cl", "Z", "-1")])
+            # add the tags to the
+            alignment.tags.extend(tags)
+            writer.write(alignment)
+
+
 def add_sam_tags(args):
     """Add single cell sam tags."""
     # open the input sam file
@@ -28,6 +70,12 @@ def add_sam_tags(args):
     stream = open(args.welllist, "rt")
     wells = read_well_list(stream)
     stream.close()
+
+    # Annotate the alignments
+    annotater = Annotate(wells)
+    annotater.wbcidx = args.barcode_field
+    annotater.umiidx = args.umi_field
+    annotater.add_tags(instream, outstream)
 
     # close the input files
     if instream is not sys.stdin:
